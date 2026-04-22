8d170a3c9ac0e73cfc014d7d3ff4ad709d23ef17
Siddhes Das <siddhesdas2020@gmail.com>
2026-04-23 02:00:36 +0530

    swirling around variables and arithmetic operation

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,3 +1,26 @@
 fun main(){
-    println("Hello world")
+    //println("Hello world")
+
+    //var myVariable: Int = 7
+    //println(myVariable)
+
+    //THIS OR THAT
+
+    //var myVariable = 7
+    //println("The value of our variable is: $myVariable")
+
+
+//    val myVariable = 7
+    //myVariable = 1 // this will give error cause after val myVariable after that the value of the variable cannot be changed
+//    println("The value of our variable is: $myVariable")
+
+//    var myVariable: Boolean = true
+//    println("The value of our variable is: $myVariable")
+
+    var name: String = "Siddhes"
+    var age: Int = 22
+    var country: String = "India"
+
+    print("My name is $name and i am $age years old and i am from $country")
+
 }
---
+++ src/tutorials2.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val x = 15
+    val y = 7
+    val result = x + y
+    println(result)
+
+    var z = 12
+    z = z + 10
+    println("The result is ${z + y}")
+}
