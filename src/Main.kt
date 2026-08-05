604e6bce75a02bbf3754db7e1636c6a609dbfb4c
SidEnpassant <siddhesdas2020@gmail.com>
2026-08-05 20:21:16 +0530

    kotlin continuation

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 fun main() {
     val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+
+    // FUNDAMENTAL OR PRIMITIVE DATATYPES
+    //    val x = 5 OR
+    // val x: Int = 5 // integer
+    //    val x: Float = 5f // float
+    //    val x: Double = 5.2222  // double numbers
+    //    val x: Boolean = true // Boolean
+//    var x: String = "Siddhes" //String
+   // can take var when value changing later on in code
+//    x = "Bye Bye"
+
+
+
+//    // ARITHMETIC OPERATORS
+//    val x = 10
+//    val y = 5
+//    println(x + y)
+//    println(x / y)
+//    println(x * y)
+//    println(x % y)
+//
+//    // COMPARISON OPERATORS
+//    println(x == y)
+//    println(x > y)
+//    println(x < y)
+//    println("Is $x an even number?: ${x % 2 == 0}")
+
+
+    // LOGICAL OPERATORS
+//    val x = 12
+//    val y = 11
+////    val areBothEven = x % 2 == 0 && y % 2 == 0
+////    val areBothEven = x % 2 == 0 || y % 2 == 0
+//    val areBothEven = x % 2 == 0 || y % 2 == 0 && x + y == 25 // && is prioritized here above ||
+//    println(areBothEven)
+
+
+    // USER INPUT
+//    println("Enter a number:")
+//    val input = readln()
+////    val inputAsInteger = input.toInt()
+//
+//    val inputAsInteger = input.toIntOrNull() ?: 0// nullability
+//    // OR
+////    val inputAsInteger = input.toIntOrNull()?.inc()// nullability
+//
+////    val isEven = inputAsInteger % 2 == 0
+////    println("Is the number even? $isEven")
+//    println("Integer is $inputAsInteger")
+//
+//    val isEven = inputAsInteger!! % 2 == 0 // !!
+//    println("Is the number even? $isEven")
+
+
+
+    //IF STATEMENTS
+
+    println("Enter a number:")
+    val input = readln()
+    val inputAsInteger = input.toIntOrNull()
+
+    if(inputAsInteger != null){
+        val isEven = inputAsInteger % 2 == 0
+        if(isEven){
+            println("The number is even !")
+        }else{
+            println("The number is odd!")
+        }
+        println("Is the number even? $isEven")
+    }else{
+        println("Dude, enter a valid number:")
     }
+
 }
